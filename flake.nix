86672cb5be5fd97a0bd5dd243b5d7db987466b7a
ChunHou20c <youmustbekiddingmeshit@gmail.com>
2024-03-07 15:54:16 +0800

    update flake.nix and flake.lock

--- flake.nix
+++ flake.nix
@@ -5,9 +5,22 @@
     nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-unstable";
   };
 
-  outputs = { self, nixpkgs }: {
+  outputs = { self, nixpkgs }: 
+  let
+      pkgs = import nixpkgs { system = "x86_64-linux"; };
+  in 
+  {
 
+    devShells.x86_64-linux = {
+
+      default = pkgs.mkShell {
+        packages = with pkgs; [
+          gcc
+          glm
+          glfw
+          glew
+        ];
+      };
+    };
   };
-
-
 }
